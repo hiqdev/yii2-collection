7b5e01e4d249e263eae3bdc8ab485aa9cf7f76c4
Andrii Vasyliev <sol@hiqdev.com>
2015-04-26 20:45:48 +0000

    php-cs-fixed

--- CollectionTrait.php
+++ CollectionTrait.php
@@ -4,7 +4,8 @@
  * HiQDev Collection for Yii 2.
  *
  * @link      http://hiqdev.com/yii2-collection
- * @license   http://hiqdev.com/yii2-collection/license
+ * @package   yii2-collection
+ * @license   BSD 3-clause
  * @copyright Copyright (c) 2015 HiQDev
  */
 
--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,10 +1,11 @@
 <?php
-/**
- * HiQDev Collection for Yii 2
+
+/*
+ * HiQDev Collection for Yii 2.
  *
- * @package   yii2-collection
  * @link      http://hiqdev.com/yii2-collection
- * @license   http://hiqdev.com/yii2-collection/license
+ * @package   yii2-collection
+ * @license   BSD 3-clause
  * @copyright Copyright (c) 2015 HiQDev
  */
 
